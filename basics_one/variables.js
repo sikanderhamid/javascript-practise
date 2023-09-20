c78f605709a37ce3478da5cc46ac245149bdbb91
sikanderhamid <134534410+sikanderhamid@users.noreply.github.com>
2023-09-20 17:12:17 +0000

    variable file commit

---
+++ basics_one/variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "sikander@gmail.com"
+var accountPassword = 12345678
+accountCity = "Gujrat"
+//accountId = 568// not allowed
+accountEmail = "awan@gmail.com"
+accountPassword = 35645
+accountCity = "Lahore"
+let accountState;
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
