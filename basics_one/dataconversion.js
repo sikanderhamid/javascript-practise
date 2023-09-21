fd650e3d2ab78cd29f331b5f007c1443345babe4
sikanderhamid <134534410+sikanderhamid@users.noreply.github.com>
2023-09-21 18:13:51 +0000

    dataconversion commit

---
+++ basics_one/dataconversion.js
@@ -0,0 +1,30 @@
+let score = 50
+let age = "80"
+let value = "Sikander"
+let numberOne = Number(age)
+let newval = String(score)      
+//console.log(typeof age)
+//console.log(typeof numberOne)
+//console.log(typeof newval)
+
+let val = 60
+let negval = -val
+//console.log(negval)//
+
+let str1 = "Sikander"
+let str2 = " Hamid"
+let str3 = str1+str2
+//console.log(str3)
+
+//console.log(2 + 2)
+//console.log("2"+"2")
+//console.log("3" + 2)
+//console.log(3 + "2")
+//console.log(6 + 2 +"8")
+//console.log("6" + 2 + 5)
+
+let counter = 300
+//counter++;
+//console.log(counter)
+++counter;
+console.log(counter)
