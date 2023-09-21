31fe8af73eef7d0f8d8c11242289760d756ba6ef
sikanderhamid <134534410+sikanderhamid@users.noreply.github.com>
2023-09-21 17:03:16 +0000

    datatypes

---
+++ basics_one/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+//alert(2+2)// we are using node js not browser
+// symbol
+// number
+// bigInt
+//boolean
+// string
+// null
+// undefined
+// object
+let age = 44
+console.log(typeof "Sikander")
+console.log(typeof age)
+console.log(typeof undefined)
+
