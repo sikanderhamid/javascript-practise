24cc304b8834c596ea8fbaeccebf4b2bd8110256
sikanderhamid <134534410+sikanderhamid@users.noreply.github.com>
2023-09-23 18:26:23 +0000

    math

---
+++ basics_one/math.js
@@ -0,0 +1,22 @@
+const score = 500
+//console.log(score)
+
+const balance = new Number(300)
+//console.log(balance);
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
+const number = 35.648
+//console.log(number.toPrecision(3));
+
+const hundred = 100000
+//console.log(hundred.toLocaleString());
+
+//console.log(Math.round(4.9))
+//console.log(Math.floor(5.8))
+//console.log(Math.abs(-9))
+//console.log(Math.random());
+//console.log(Math.min(2,6,8,4,9))
+//console.log(Math.maz(2,6,5,8,9))
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1)+min))
