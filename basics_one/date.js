da5fc79599b6e440fc5e8133f1c782e51407b2fa
sikanderhamid <134534410+sikanderhamid@users.noreply.github.com>
2023-09-24 09:58:26 +0000

    date

---
+++ basics_one/date.js
@@ -0,0 +1,10 @@
+const myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toLocaleString);
+//console.log(myDate.toDateString);
+
+let urDate = new Date(2023,0,5,3,8)
+//console.log(urDate.toLocaleString());
+
+let createDate = new Date("01-12-2014")
+console.log(createDate.toLocaleString());
