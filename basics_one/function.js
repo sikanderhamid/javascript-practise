4793a34472f15eb29a4015024342d2430f8c224b
sikanderhamid <134534410+sikanderhamid@users.noreply.github.com>
2023-09-30 17:58:20 +0000

    function

---
+++ basics_one/function.js
@@ -0,0 +1,10 @@
+function addNumbers(a,b){
+    return(a+b)
+}
+let res = addNumbers(3,4)
+//console.log(res);
+
+function logUser(username){
+    return `My name is ${username} and I am not a terrorist`
+}
+console.log(logUser("sikander"))
