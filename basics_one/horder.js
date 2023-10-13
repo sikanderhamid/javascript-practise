54aebd8f9d8386f70142e8051c9460faf5944a36
sikanderhamid <134534410+sikanderhamid@users.noreply.github.com>
2023-10-13 14:28:41 +0000

    horder

--- basics_one/horder.js
+++ basics_one/horder.js
@@ -31,5 +31,44 @@ for(const key in myObj){
 }
 
 for(const key in myObj){
-    console.log(`my fav ${key} is ${myObj[key]}`);
+    //console.log(`my fav ${key} is ${myObj[key]}`);
 }
+
+const langs = ["C++","JAVA","PYTHON","JAVASCRIPT"]
+langs.forEach(function(val){
+    //console.log(val);
+})
+
+langs.forEach((vals)=>{
+    //console.log(vals);
+})
+
+function readMe(item){
+    //console.log(item);
+}
+//langs.forEach(readMe)
+
+langs.forEach((items,index,arr)=>{
+    //console.log(items,index,arr);
+})
+
+const data = [
+    {
+        lang:"JAVA",
+        version:"2.0.3"
+    },
+    {
+        lang:"FORTRAN",
+        version:"2.3.8"
+    },
+    {
+        lang:"PYTHON",
+        version:"9.7.8"
+    }
+
+]
+
+data.forEach((items)=>{
+    console.log(items.lang,"and",items.version);
+
+})
